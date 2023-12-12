08b14babfaf05f6c0f98f162a3429690ac9c1669
Ammen1 <amenguda@gmail.com>
2023-12-12 09:56:39 +0300

    urls for services

--- web/core/base/urls.py
+++ web/core/base/urls.py
@@ -5,7 +5,15 @@ from . views import *
 app_name = 'base'
 
 urlpatterns = [
+    # Admin
     path('admin/create/', CreateService.as_view(), name='createservice'),
     path('admin/listcreate/', ServiceList.as_view(), name='listservice'),
-    
+    path('admin/detailservice/<int:id>/',
+         ServiceDaitle.as_view(), name='detailservice'),
+    path('admin/editservices/<int:pk>/',
+         EditService.as_view(), name='editservices'),
+    path('admin/deleteservice/<int:pk>/',
+         DeleteService.as_view(), name='deleteservice')
+
+
 ]

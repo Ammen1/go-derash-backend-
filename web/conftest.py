0bc351aa8f31e3ad11db56e8c28cd7f35982645f
Ammen1 <amenguda@gmail.com>
2024-01-05 10:17:07 +0300

    git push

--- web/conftest.py
+++
@@ -1,5 +0,0 @@
-pytest_plugins = [
-    "core.tests.inventory_fixtures",
-    "core.tests.promotion_fixtures",
-    "core.tests.api_client",
-]
--- web/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-APP_PORT=${PORT:-8000}
-cd /app/
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm go-derashbackend.wsgi:application --bind "0.0.0.0:${APP_PORT}"

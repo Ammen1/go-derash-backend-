e5eea8cd1fec44c55315ef6a8ee2ca9ef442b069
Ammen1 <amenguda@gmail.com>
2024-01-10 22:48:46 +0300

    Crud for battery services

--- web/core/battery/urls.py
+++ web/core/battery/urls.py
@@ -10,26 +10,27 @@ urlpatterns = [
          DetailBattery.as_view(), name="detailbattery"),
     path('user/detelebattery/<int:pk>/',
          DeleteBattery.as_view(), name="detelebattery"),
-    paht('user/updatebattery/<int:pk>/',
+    path('user/updatebattery/<int:pk>/',
          UpdateBattery.as_view(), name="updatebattery"),
     # ursl for Brand
-    path('user/batterybrand/', BatteryBrand.as_view(), name='batterybrand'),
+    path('user/batterybrand/', CreatBrand.as_view(), name='batterybrand'),
     path('user/allbrand/', ListBrand.as_view(), name="allbrand"),
     path('user/detailbrand/<int:pk>/',
          DetailBrand.as_view(), name="detailbrand"),
     path('user/detelebrand/<int:pk>/',
          DeleteBrand.as_view(), name="detelebrand"),
-    paht('user/updatebrand/<int:pk>/',
+    path('user/updatebrand/<int:pk>/',
          UpdateBrand.as_view(), name="updatebrand"),
 
     # urls for Category
-    path('user/batterycategory/', BatteryCategory.as_view(), name='batterycategory'),
+    path('user/batterycategory/',
+         CreatBatteryCategory.as_view(), name='batterycategory'),
     path('user/allcategory/', ListBatteryCategory.as_view(), name="allcategory"),
     path('user/detailcategory/<int:pk>/',
          DetailBatteryCategory.as_view(), name="detailcategory"),
     path('user/detelecategory/<int:pk>/',
          DeleteBatteryCategory.as_view(), name="detelecategory"),
-    paht('user/updatecategory/<int:pk>/',
+    path('user/updatecategory/<int:pk>/',
          UpdateBatteryCategory.as_view(), name="updatecategory"),
 
 ]

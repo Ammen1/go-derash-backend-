11723f960c7125391aa5f8127e61701f7f269915
Ammen1 <amenguda@gmail.com>
2024-01-14 23:06:20 +0300

    settings backe

--- web/core/settings.py
+++ web/core/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
 ENV_ALLOWED_HOST = os.environ.get("ENV_ALLOWED_HOST")
-ALLOWED_HOSTS = ["gooderash.com", "www.gooderash.com"]
+ALLOWED_HOSTS = []
+# "gooderash.com", "www.gooderash.com"
 if ENV_ALLOWED_HOST:
     ALLOWED_HOSTS = [ENV_ALLOWED_HOST]
 
